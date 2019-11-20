b8d3839b85838e0b5597347793436e6face62174
seedmonn <seedmp322@yandex.ru>
2019-11-20 19:27:51 +0300

    final version

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
+#include<locale.h>
 #define LEN 100
 #define SPC 20
-
 struct data
 {
 	char name[LEN];
@@ -165,4 +165,366 @@ void save(struct data* head)
 		current = current->ptr;
 	}
 	fclose(fout);
+}
+void addDoc(struct data** head, int* cnt)
+{
+	char spec[6][20] = { "Хирург","Офтальмолог","Терапевт","Педиатр","ЛОР","Стоматолог" };
+	char days[5][20] = { "Понедельник","Вторник","Среда","Четверг","Пятница" };
+	char* ptr;
+	int i;
+	int callback_int;
+	char callback_char[10];
+	int flag;
+
+	struct data* current;
+
+	if (!*cnt)
+	{
+		current = *head = init();
+		(*cnt)++;
+	}
+	else
+	{
+		current = appendlist(head);
+		(*cnt)++;
+	}
+	system("cls");
+	printf("Введите ФИО врача:\nФИО: ");
+	flag = 0;
+	do {
+		if (flag)
+			printf("Введено неправильное имя. Введите имя в формате Фамилия И.О. ");
+		scanf("\n%[^\n]", &current->name);
+		ptr = &current->name;
+		while (*ptr != '\0')
+		{
+			if (!(*ptr >= 'а' && *ptr <= 'я' || *ptr == ' ' || *ptr == '.' || *ptr >= 'А' && *ptr <= 'Я'))
+			{
+				flag = 1;
+				break;
+			}
+			else
+			{
+				flag = 0;
+			}
+			ptr++;
+		}
+	} while (flag);
+	flag = 0;
+	system("cls");
+	printf("Введите специальность врача:\n");
+	for (i = 0; i < 6; i++)
+	{
+		printf("[%d]%s\n", i + 1, spec[i]);
+	}
+	printf("Выберите специальность врача (1-6): ");
+	flag = 0;
+	do {
+		if (flag)
+			printf("Введено неправильное значение. Введите от 1 до 6: ");
+		scanf("%s", &callback_char);
+		flag = 1;
+		callback_int = atoi(callback_char);
+	} while (callback_int < 1 || callback_int > 6);
+	strcpy(current->spec, spec[callback_int - 1]);
+	system("cls");
+	printf("Введите номер кабинета: ");
+	scanf("%d", &current->num);
+	system("cls");
+	for (i = 0; i < 5; i++)
+	{
+		printf("[%d]%s\n", i + 1, days[i]);
+	}
+	printf("Выберите день работы врача (1-5): ");
+	flag = 0;
+	do {
+		if (flag)
+			printf("Введено неправильное значение. Введите от 1 до 5: ");
+		scanf("%s", &callback_char);
+		flag = 1;
+		callback_int = atoi(callback_char);
+	} while (callback_int < 1 || callback_int > 5);
+	current->day = callback_int;
+	system("cls");
+	printf("Введите время начала работы: ");
+	flag = 0;
+	do {
+		if (flag)
+			printf("Введено неправильное значение. Введите от 8 до 24: ");
+		scanf("%s", &callback_char);
+		flag = 1;
+		callback_int = atoi(callback_char);
+		current->st_vis = callback_int;
+	} while (callback_int < 8 || callback_int > 24);
+	system("cls");
+	printf("Введите время конца работы: ");
+	flag = 0;
+	do {
+		if (flag)
+			printf("Введено неправильное значение. Введите от 16 до 24: ");
+		scanf("%s", &callback_char);
+		flag = 1;
+		callback_int = atoi(callback_char);
+		current->end_vis = callback_int;
+	} while (callback_int < 16 || callback_int > 24);
+}
+void edit(struct data* current)
+{
+	int i;
+	char vibor[2];
+	int ivibor = 0;
+	int flag;
+	char* ptr;
+	char spec[6][20] = { "Хирург","Офтальмолог","Терапевт","Педиатр","ЛОР","Стоматолог" };
+	char days[5][20] = { "Понедельник","Вторник","Среда","Четверг","Пятница" };
+	system("cls");
+	printf("Старое ФИО врача: %s \n", current->name);
+	printf("Введите новое ФИО врача:\nФИО: ");
+	flag = 0;
+	do {
+		if (flag)
+			printf("Введено неправильное имя. Введите имя в формате Фамилия И.О. : ");
+		scanf("\n%[^\n]", &current->name);
+		ptr = &current->name;
+		while (*ptr != '\0')
+		{
+			if (!(*ptr >= 'а' && *ptr <= 'я' || *ptr == ' ' || *ptr == '.' || *ptr >= 'А' && *ptr <= 'Я'))
+			{
+				flag = 1;
+				break;
+			}
+			else
+			{
+				flag = 0;
+			}
+			ptr++;
+		}
+	} while (flag);
+	flag = 0;
+	system("cls");
+	printf("Старая специальность врача: %s \n", current->spec);
+	printf("Введите новую специальность врача:\n");
+	for (i = 0; i < 6; i++)
+	{
+		printf("[%d]%s\n", i + 1, spec[i]);
+	}
+	printf("Выберите специальность врача (1-6): ");
+	flag = 0;
+	do {
+		if (flag)
+			printf("Введено неправильное значение. Введите от 1 до 6: ");
+		scanf("%s", &vibor);
+		ivibor = atoi(vibor);
+		flag = 1;
+	} while ((ivibor < 1) || (ivibor > 6));
+	strcpy(current->spec, spec[ivibor - 1]);
+	flag = 0;
+	system("cls");
+	printf("Старый номер кабинета: %d\n", current->num);
+	printf("Введите новый номер кабинета: ");
+	scanf("%d", &current->num);
+	system("cls");
+	printf("Бывший день работы врача: %s \n", days[current->day - 1]);
+	for (i = 0; i < 5; i++)
+	{
+		printf("[%d]%s\n", i + 1, days[i]);
+	}
+	printf("Выберите день работы врача (1-5): ");
+	flag = 0;
+	do {
+		if (flag)
+			printf("Введено неправильное значение. Введите от 1 до 5: ");
+		scanf("%d", &vibor);
+		flag = 1;
+	} while ((vibor < 1) || (vibor > 5));
+	current->day = vibor;
+	system("cls");
+	printf("Введите время начала работы: ");
+	scanf("%d", &current->st_vis);
+	system("cls");
+	printf("Введите время конца работы: ");
+	scanf("%d", &current->end_vis);
+}
+int main(void)
+{
+	system("chcp 1251");
+	system("cls");
+	char *locale = setlocale(LC_ALL, "");
+	struct data* head = NULL;
+	struct data* current = NULL;
+	int vibor;
+	int flag = 0;
+	int count = 0, i = 0;
+	char days[5][20] = { "Понедельник","Вторник","Среда","Четверг","Пятница" };
+	char spec[6][20] = { "Хирург","Офтальмолог","Терапевт","Педиатр","ЛОР","Стоматолог" };
+	char main;
+	input(&head, &count);
+	do
+	{
+		system("cls");
+		printf("[0] Выйти из программы\n[1] Добавить нового врача\n[2] Редактировать расписания\n[3] Уволить врача\n[4] Вывести врачей в алфавитном порядке\n[5] Работа кабинета по дням недели\n[6] Подбор врача\nВыберите пункт из списка : ");
+		scanf("%c", &main);
+		switch (main)
+		{
+		case '1':
+			addDoc(&head, &count);
+			break;
+		case '2':
+			system("cls");
+			if (!count)
+			{
+				printf("Нет врачей\n");
+				system("pause");
+				break;
+			}
+			i = 0;
+			current = head;
+			while (current != NULL)
+			{
+				printf("[%d] %s - %s\n", i + 1, current->name, current->spec);
+				i++;
+				current = current->ptr;
+			}
+			printf("Выберите врача для изменения: ");
+			scanf("%d", &vibor);
+			current = head;
+			for (i = 0; i < vibor - 1; i++)
+			{
+				current = current->ptr;
+			}
+			edit(current);
+			break;
+		case '3':
+			system("cls");
+			if (!count)
+			{
+				printf("Нет врачей\n");
+				system("pause");
+				break;
+			}
+			current = head;
+			i = 0;
+			while (current != NULL)
+			{
+				printf("[%d] %s\n", i + 1, current->name);
+				i++;
+				current = current->ptr;
+			}
+			printf("Выберите врача для увольнения: ");
+			scanf("%d", &vibor);
+			current = head;
+			for (i = 0; i < vibor - 1; i++)
+			{
+				current = current->ptr;
+			}
+			if (vibor == 1)
+			{
+				head = deleteHead(current);
+
+			}
+			else
+			{
+				deleteDoc(current, head);
+			}
+			count--;
+			break;
+		case '4':
+			if (!count)
+			{
+				printf("Нет врачей\n");
+				system("pause");
+				break;
+			}
+			sort(head, count);
+			current = head;
+			i = 1;
+			while (current != NULL)
+			{
+				printf("------[%d]------\n", i);
+				printf("Специальность: %s\n", current->spec);
+				printf("Имя: %s\n", current->name);
+				printf("Кабинет: %d\n", current->num);
+				printf("День: %s\n", days[(current->day) - 1]);
+				printf("Начало/Конец приема: с %d до %d\n", current->st_vis, current->end_vis);
+				i++;
+				current = current->ptr;
+			}
+			system("pause");
+			break;
+		case '5':
+			system("CLS");
+			if (!count)
+			{
+				printf("Нет врачей\n");
+				system("pause");
+				break;
+			}
+			printf("Введите номер кабинета: ");
+			scanf("%d", &vibor);
+			system("CLS");
+			printf("=========================\n");
+			printf("Кабинет ¹ %d\n", vibor);
+			printf("=========================\n");
+			for (i = 0; i < 5; i++)
+			{
+				printf("-------------------------\n");
+				printf("%s\n", days[i]);
+				printf("-------------------------\n");
+				current = head;
+				while (current != NULL)
+				{
+					if (current->num == vibor && current->day == i)
+						printf("%d - %d\n", current->st_vis, current->end_vis);
+					current = current->ptr;
+				}
+				printf("\n");
+			}
+			system("pause");
+			break;
+		case '6':
+			system("CLS");
+			if (!count)
+			{
+				printf("Нет врачей\n");
+				system("pause");
+				break;
+			}
+			for (i = 0; i < 6; i++)
+			{
+				printf("[%d]%s\n", i + 1, spec[i]);
+			}
+			printf("Выберите специальность врача (1-6): ");
+			flag = 0;
+			do {
+				if (flag)
+					printf("Введено неправильное значение. Введите от 1 до 6: ");
+				scanf("%d", &vibor);
+				flag = 1;
+			} while ((vibor < 1) || (vibor > 6));
+			system("CLS");
+			current = head;
+			i = 1;
+			while (current != NULL)
+			{
+				if (!strcmp(current->spec, spec[vibor - 1]))
+				{
+					printf("------[%d]------\n", i);
+					printf("Специальность: %s\n", current->spec);
+					printf("Имя: %s\n", current->name);
+					printf("Кабинет: %d\n", current->num);
+					printf("День: %s\n", days[(current->day) - 1]);
+					printf("Начало/Конец приема: с %d до %d\n", current->st_vis, current->end_vis);
+					i++;
+				}
+				current = current->ptr;
+			}
+			system("pause");
+			break;
+		default:
+			break;
+		}
+	} while (main != '0');
+	save(head);
+	freeStruct(head);
+	return 0;
 }
